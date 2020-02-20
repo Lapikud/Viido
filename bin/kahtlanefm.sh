f1c40af2f3b98a44e09e2dfa03ceafefa3edc06a
Arti Zirk <arti.zirk@gmail.com>
2020-02-20 13:13:36 +0200

    Add package hints for kahtlanefm streamer

--- bin/kahtlanefm.sh
+++ bin/kahtlanefm.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Needs gstreamer1.0-tools for gst-launch and gst-plugins-base for vorbis encoder
+
 while true; do
 GST_DEBUG=2 gst-launch-1.0 pulsesrc device=alsa_output.pci-0000_00_09.0.analog-stereo.monitor ! audioconvert ! audio/x-raw,channels=2 ! taginject tags="title=Viido,artist=Lapikud" ! vorbisenc quality=0.8 ! oggmux ! shout2send ip=kahtlane.eu port=8000 username= password= mount=live
 sleep 10
